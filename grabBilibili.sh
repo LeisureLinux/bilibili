d0c1cada9b4c220b818aa791717642487ccaf974
Albert Xu <axu@yj777.cn>
2021-11-09 13:32:20 +0800

    try to convert date in jq command

--- grabBilibili.sh
+++ grabBilibili.sh
@@ -2,11 +2,27 @@
 # 抓取B站自己已经发布的视频的信息：视频标题，视频 BVID，发布时间（timestamp）
 # COOKIE 变量是从登录到B站后进入浏览器的开发模式
 # 选择：网络->Fetch/XHR->标头->请求标头->cookie 的值
-COOKIE="sample_cookie_data;b_ut=-1; i-wanna-go-back=-1; _uuid=3BE; "
-RES="bilibili.csv"
-cat /dev/null >$RES
-# 共抓取13页
-for p in $(seq 1 13); do
-	URL='https://member.bilibili.com/x/web/archives?status=is_pubing,pubed,not_pubed&pn='$p'&ps=50&coop=1&interactive=1'
-	curl -o - -b "$COOKIE" "$URL" | jq .data.arc_audits | jq -r '.[]|.Archive|[.title,.bvid,.ptime]|@csv' >>$RES
-done
+[ -z "$1" ] && echo "Syntax: $0 \"\$COOKIE\" " && exit 1
+COOKIE="$1"
+RES="bilibili.json"
+bArch='https://member.bilibili.com/x/web/archives?status=pubed&ps=10&coop=1&interactive=1&pn='
+
+grabHistory() {
+	# 抓取历史存量
+	# 共抓取13页
+	cat /dev/null >$RES
+	for p in $(seq 1 13); do
+		URL=${bArch}$p
+		curl -s -o - -b "$COOKIE" "$URL" | jq .data.arc_audits | jq -r '.[]|.Archive|[.title,.bvid,.ptime]|@csv' >>$RES
+	done
+}
+
+grabFirstPage() {
+	URL=${bArch}1
+	# curl -s -o - -b "$COOKIE" "$URL" | jq .data.arc_audits | jq -r '.[]|.Archive|[.title,.bvid,.ptime]|@csv' >$RES
+	# curl -s -o - -b "$COOKIE" "$URL" | jq .data.arc_audits | jq '.[]|.Archive' | jq -n '{bvid,title,cover,tag,duration,desc,(.ptime | strftime("%Y-%m-%d"))}' >$RES
+	curl -s -o - -b "$COOKIE" "$URL" | jq .data.arc_audits | jq '.[]|.Archive' | jq '{bvid,title,cover,tag,duration,desc}"",ptime|"(todate)"}' >$RES
+}
+
+# Main.
+grabFirstPage
