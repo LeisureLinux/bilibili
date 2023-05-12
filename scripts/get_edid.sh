b0eeff007736c0e6159113aada821489371136b8
Albert Xu <albertxu@freelamp.com>
2023-05-12 20:40:44 +0800

    add inxi explain

--- scripts/get_edid.sh
+++ scripts/get_edid.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # 使用 edid-decode 读取显示器序列号，型号，品牌名称，出厂日期(周)，厂商名称
 # Last modified: Fri May 12 20:20:21 CST 2023
+# Can also use inxi -Gxxx command to get the required information
 # Comment in "HIDE" to show product serial number in output
-HIDE="--hide-serial-numbers"
+# HIDE="--hide-serial-numbers"
 for m in $(ls /sys/class/drm/*/edid); do
 	dev=$(basename $(dirname $m))
 	edid=$(edid-decode -c $HIDE $m 2>/dev/null | grep -E \
