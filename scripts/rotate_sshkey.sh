904246b667b3b57ed630a4a2bed89bd044e9de59
Albert Xu <albertxu@freelamp.com>
2023-12-17 22:04:19 +0800

    fix the sed bug

--- scripts/rotate_sshkey.sh
+++ scripts/rotate_sshkey.sh
@@ -19,7 +19,7 @@ fi
 PRIV="$HOME"/.ssh/id_rsa
 PUB="$PRIV".pub
 # grab the last 8 chars in key part as key.
-old_key=$(awk '{print substr($(NF - 1 ), length($(NF -1 )) - 8 )}' "$PUB")
+oldkey=$(awk '{print substr($(NF - 1 ), length($(NF -1 )) - 8 )}' "$PUB")
 # old_fp=$(ssh-keygen -l -f "$PUB")
 # old_key_id=$(awk '{print $NF}' "$PUB")
 mv "$PRIV" "$PRIV.old"
@@ -28,7 +28,7 @@ mv "$PUB" "$PRIV.old.pub"
 yes | ssh-keygen -t rsa -N '' -f "$PRIV"
 # new_key=$(awk '{print substr($(NF - 1 ), length($(NF -1 )) - 8 )}' "$PUB")
 # new_key_id=$(awk '{print $NF}' "$PUB")
-# echo "Old Key ID: $old_key_id, Old Key: $old_key"
+echo "Old Key: $old_key"
 # echo "New Key ID: $new_key_id, New Key: $new_key"
 # Verify old key still working
 if ! ssh -o PasswordAuthentication=no -i $PRIV.old $host echo "Old Key Verified OK"; then
@@ -37,8 +37,9 @@ if ! ssh -o PasswordAuthentication=no -i $PRIV.old $host echo "Old Key Verified 
 fi
 # Add New Key
 cat $PUB | ssh -o PasswordAuthentication=no -i $PRIV.old $host tee -a $AUTH_KEY
-# Remove Old Key
-ssh -o PasswordAuthentication=no -i $PRIV.old $host sed -i "/$old_key/d" $AUTH_KEY
+# Remove Old Key, key character could be /+=, so use "|" in sed pattern.
+CMD="sed -i '\|$oldkey|d' $AUTH_KEY"
+ssh -o PasswordAuthentication=no -i $PRIV.old $host "$CMD"
 # Verify New Key
 if ! ssh -o PasswordAuthentication=no $host echo "New Key verified OK"; then
 	echo "Error: new key failed to add."
